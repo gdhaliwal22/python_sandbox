466f68418ec27d9b892f1027dd5486fc3088b808
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-03-10 14:19:23 -0700

    created and added to dictionary collection

--- python_sandbox_starter/dictionaries.py
+++ python_sandbox_starter/dictionaries.py
@@ -1,1 +1,25 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
+
+# Create dictionary
+
+person = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'age': 30
+}
+
+
+# Use contructor
+person2 = dict(first_name='Sara', last_name='Williams')
+print(person2, type(person2))
+
+
+# Get value
+print(person['first_name'])
+print(person.get('last_name'))
+
+
+# Add key/value
+person['phone'] = '555-5555-5555'
+
+print(person)
--- python_sandbox_starter/tuples_sets.py
+++ python_sandbox_starter/tuples_sets.py
@@ -1,5 +1,15 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
-
+fruits = ('Apple', 'Orange', "Banana")
+print(fruits[1])
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+fruits_set = {'Apples', 'Oranges', 'Mango'}
+
+# Add to set
+
+fruits_set.add('Grape')
+
+
+# Remove from set
+fruits_set.remove('Grape')
+print(fruits_set)
